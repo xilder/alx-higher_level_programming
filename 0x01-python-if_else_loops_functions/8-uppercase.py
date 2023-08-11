a52943559e46616de77d89d44d7b12834e503d36
xilder <olabowaleayobami@gmail.com>
2023-08-11 23:46:13 +0000

    adds 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        c = ord(c)
+        if c >= 97 and c <= 122:
+            c = c - 32
+        else:
+            c = c
+        print("{:c}".format(c), end="")
+    print()
+
+
