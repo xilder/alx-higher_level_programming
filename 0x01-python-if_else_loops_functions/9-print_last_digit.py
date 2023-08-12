55631e83a8099bfead858afcbba86c8e1df94562
xilder <olabowaleayobami@gmail.com>
2023-08-12 09:50:16 +0000

    modifies 9-print_last_digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    i = number % 10
+    if number < 0:
+        i = number % -(10)
+    else:
+        i = number  % 10
     print(i, end="")
+    return abs(i)
