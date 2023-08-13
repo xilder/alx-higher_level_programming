9d416739d218a233e83d322ee07bace9c8850c03
xilder <olabowaleayobami@gmail.com>
2023-08-13 14:54:41 +0000

    mod 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
         no_c_str = ""
         for i in my_string:
-            if i != "c" and i "C":
+            if i != "c" and i != "C":
                 no_c_str += i
-        return no_c_str
+        return (no_c_str)
