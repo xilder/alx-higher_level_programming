d303b034c090b27cf50de295c57235ac0ec6f692
xilder <olabowaleayobami@gmail.com>
2023-08-13 14:53:08 +0000

    adds 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        no_c_str = ""
+        for i in my_string:
+            if i != "c" and i "C":
+                no_c_str += i
+        return no_c_str
