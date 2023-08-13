a56a55697c52b8020cc9c180c9a1b544e0a23f7b
xilder <olabowaleayobami@gmail.com>
2023-08-13 14:09:38 +0000

    adds 4_new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        if idx <= 0 or idx >= len(my_list):
+            return (my_list)
+        else:
+            duplicate_list = my_list.copy()
+            duplicate_list[idx] = number
+            return (duplicate_list)
