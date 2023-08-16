5c08a7cd86be8b4b1f071df2a6dd790bfa507df5
xilder <olabowaleayobami@gmail.com>
2023-08-16 21:37:58 +0000

    modifies 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    def replacer(e):
+        return e if e != search else replace
+    return list(map(replacer, my_list))
