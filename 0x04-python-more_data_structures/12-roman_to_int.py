747bc0e2f40c5d23fd40bbeba630fcc9b8a4a700
xilder <olabowaleayobami@gmail.com>
2023-08-17 05:29:05 +0000

    adds 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    sum = 0
+    i = 0
+    if isinstance(roman_string, str):
+        while i < len(roman_string) - 1:
+            if roman[roman_string[i]] >= roman[roman_string[i + 1]]:
+                sum += roman[roman_string[i]]
+            else:
+                sum -= roman[roman_string[i]]
+            i += 1
+        sum += roman[roman_string[i]]
+        return sum
+    else:
+        return None
