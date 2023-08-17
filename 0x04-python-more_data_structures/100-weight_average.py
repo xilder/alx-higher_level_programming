8d6e63cac72084c28f4dd1d75862cbe44cd89ed0
xilder <olabowaleayobami@gmail.com>
2023-08-17 05:41:53 +0000

    adds 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,10 @@
 def weight_average(my_list=[]):
     sum1 = 0
     denom = 0
-    for i in my_list
+    if my_list:
+        for i in my_list:
+            sum1 += i[0] * i[1]
+            denom += i[1]
+        return (sum1 / denom)
+    return 0
 
