78016b254bbad8e8b3ec87b135373488f4e97b92
xilder <olabowaleayobami@gmail.com>
2023-08-29 01:43:54 +0000

    adds 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+        Args:
+            my_list_1 (list): The first list.
+            my_list_2 (list): The second list.
+            list_length (int): The number of elements to divide.
+        Returns:
+            A new list of length list_length containing all the divisions.
+    """
+    new_list = []
+    for i in range(list_length):
+        try:
+            num = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            num = 0
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(num)
+    return (new_list)
