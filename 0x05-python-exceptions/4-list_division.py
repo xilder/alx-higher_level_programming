0f70969a85d43620cc467bb90527bbd4976a9836
xilder <olabowaleayobami@gmail.com>
2023-08-29 01:54:22 +0000

    mods 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,10 +19,10 @@ def list_division(my_list_1, my_list_2, list_length):
             num = 0
         except TypeError:
             print("wrong type")
-            div = 0
+            num = 0
         except IndexError:
             print("out of range")
-            div = 0
+            num = 0
         finally:
             new_list.append(num)
     return (new_list)
