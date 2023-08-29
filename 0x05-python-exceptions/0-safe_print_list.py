c36685b4c1b8c179b794ad0b8844298cd1485ed4
xilder <olabowaleayobami@gmail.com>
2023-08-29 00:36:01 +0000

    mods 0-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,5 +19,5 @@ def safe_print_list(my_list=[], x=0):
             total += 1
         except IndexError:
             break
-        print("")
-        return(total)
+    print("")
+    return(total)
