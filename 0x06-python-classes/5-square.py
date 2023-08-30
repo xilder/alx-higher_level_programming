4411b3c6e7cb44f731b134b391b008894270c7a1
xilder <olabowaleayobami@gmail.com>
2023-08-30 22:26:29 +0000

    mods 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,7 +28,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
