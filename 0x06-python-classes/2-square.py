0ef3a609d0420428c668930a6fa47001de1569c5
xilder <olabowaleayobami@gmail.com>
2023-08-30 02:58:43 +0000

    mods 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     Args: size - represents the size of the square defined
     """
 
-    def __init__(self, size):
-        if isinstance(size, int)
+    def __init__(self, size=0):
+        if not isinstance(size, int)
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
