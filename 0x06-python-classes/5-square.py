54dc4fcf3b4b7ff1d22c5575c1fe17a351dce28b
xilder <olabowaleayobami@gmail.com>
2023-08-30 20:09:55 +0000

    mods 4-square & 5-square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """
+        Init the square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if size <= 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size )
