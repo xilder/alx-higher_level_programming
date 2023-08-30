68a600c9f2762b39eeee1fafa91b00867f83e08c
xilder <olabowaleayobami@gmail.com>
2023-08-30 03:09:04 +0000

    mods 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """Represents a square"""
-
     def __init__(self, size=0):
         """
         Init the square class
@@ -19,8 +18,9 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
-        Caculates the area of the square
+        Calculates the area of the square
         """
         return (self.__size ** 2)
