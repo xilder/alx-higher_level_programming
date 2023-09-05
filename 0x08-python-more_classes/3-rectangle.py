6bc9eadd939c9f98167f8edcd9766658321c2ad6
xilder <olabowaleayobami@gmail.com>
2023-09-05 23:37:43 +0000

    adds 3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""a class that defines a rectangle"""
+
+
+class Rectangle:
+    """
+    A rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """returns the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height * 2 + self.__width * 2)
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return("")
+        else:
+            rectangle = ""
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    rectangle += "#"
+                if i < self.__height - 1:
+                    rectangle += "\n"
+            return (rectangle)
