805f94fc3a51af39be745dc7fd1043cf086cff52
xilder <olabowaleayobami@gmail.com>
2023-09-05 23:09:29 +0000

    adds 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""an empty rectangle class"""
+
+
+class Rectangle:
+    """
+    A rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """returns the width"""
+            return self.__width
+
+        @width.setter(self, value):
+            """sets the width"""
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """returns the height"""
+            return self.__height
+        
+        @height.setter
+        """sets the height"""
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
