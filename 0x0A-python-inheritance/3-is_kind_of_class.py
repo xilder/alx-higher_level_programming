3e0d8516fc471f7f1f0241beff7d4bcce9539bc7
xilder <olabowaleayobami@gmail.com>
2023-09-11 23:25:06 +0000

    adds 3-is_kind_of class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""checks for the instance of an object against a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    compares an instance against a class
+    """
+
+    return (isinstance(obj, a_class))
