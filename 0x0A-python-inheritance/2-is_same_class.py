9baa84bd2e63996bb02b27aadfe73558449cf10d
xilder <olabowaleayobami@gmail.com>
2023-09-11 23:15:56 +0000

    adds 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""function to get the class of an obj"""
+
+
+def is_same_class(obj, a_class):
+    """return True if obj and class are the same type"""
+    return (type(obj) == a_class)
