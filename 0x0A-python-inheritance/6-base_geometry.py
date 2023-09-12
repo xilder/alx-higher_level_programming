6ea5bdeb2204256c75442ea2f0bbaf197e4e4da5
xilder <olabowaleayobami@gmail.com>
2023-09-12 01:23:35 +0000

    adds 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""An empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """represents a base geometry"""
+    def area(self):
+        """method not implemented"""
+        raise Exception("area() is not implemented")
