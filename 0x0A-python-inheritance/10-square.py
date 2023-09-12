d9c7e6cb3a9aad680a7ac418794ca5c91f8f7f42
xilder <olabowaleayobami@gmail.com>
2023-09-12 10:54:40 +0000

    mods 10-square.py and 11-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-"""a square subclass"""
+"""a rectangle subclass called square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represents a square"""
+
     def __init__(self, size):
         """inits the Square"""
         self.integer_validator("size", size)
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,11 +5,16 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """A square class"""
+
     def __init__(self, size):
         """inits the Square"""
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """string representation pf the square class"""
+
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
