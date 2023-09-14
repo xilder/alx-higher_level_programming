c2bfd15e5ceb3870e741740152bdb560125cfdd1
xilder <olabowaleayobami@gmail.com>
2023-09-14 16:35:26 +0000

    mods 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -16,5 +16,5 @@ def append_after(filename="", search_string="", new_string=""):
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, 'w', encoding='UTF-8') as h: 
+    with open(filename, 'w', encoding='UTF-8') as h:
         h.write(text)
