6bb90d3e8749cfe66d66dd327e5ac7ad65ea5d35
xilder <olabowaleayobami@gmail.com>
2023-10-19 00:25:20 -0700

    adds  100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN (
+	SELECT tv_genres.name
+	FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+	INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+	WHERE tv_shows.title = "Dexter"
+)
+ORDER BY tv_genres.name ASC
+
