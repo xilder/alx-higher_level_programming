681a979bfc23bbc80d77c0cf89fbf7ab1861255c
xilder <olabowaleayobami@gmail.com>
2023-11-13 08:07:30 -0800

    adds 2-arguments.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const threeLang = "C is fun\nPython is cool\nJavaScript is amazing";
+const threeLang = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(threeLang);
