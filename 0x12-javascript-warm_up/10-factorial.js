f5a399acd8cb84ed92a62198eb8e249d2e68749a
xilder <olabowaleayobami@gmail.com>
2023-11-13 11:20:05 -0800

    update ./10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-let total = 1;
 
-function factorial(number) {
+function factorial (number) {
   if (number === 0) {
     return 1;
   }
-  return number * factorial(number - 1)
+  return number * factorial(number - 1);
 }
 
 if (num) {
