662462c77a29a8bb76106f0d05ec5746041fef6a
xilder <olabowaleayobami@gmail.com>
2023-11-13 14:09:25 -0800

    updates ./103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
-	  type: 'object',
-	  value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 
-myObject["incr"] = () => ++myObject["value"];
+myObject.incr = () => ++myObject.value;
 
 myObject.incr();
 console.log(myObject);
