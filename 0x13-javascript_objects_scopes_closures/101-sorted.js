d7816f1e0451ccdbe17d8c5fb89714c353966706
xilder <olabowaleayobami@gmail.com>
2023-11-14 15:49:29 -0800

    adss 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const newDict = {}
+const values = Object.entries(dict);
+
+values.forEach(([key, value]) => {
+  if (value in newDict) {
+    newDict[value].push(key.toString());
+  } else {
+    newDict[value] = [];
+    newDict[value].push(key.toString());
+  }
+});
+
+console.log(newDict);
