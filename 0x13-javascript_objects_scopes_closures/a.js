4a1bb6e440031764e25713cf57bfe48c8bc348b2
xilder <olabowaleayobami@gmail.com>
2023-11-14 12:38:21 -0800

    adds  9-logme.js

--- 0x13-javascript_objects_scopes_closures/a.js
+++ 0x13-javascript_objects_scopes_closures/a.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+const logMe = require('./9-logme').logMe;
 
-const esrever = require('./8-esrever').esrever;
-
-console.log(esrever([1, 2, 3, 4, 5]));
-console.log(esrever(["School", 89, { id: 12 }, "String"]));
+logMe("Hello");
+logMe("Best");
+logMe("School");
 
