a3b71db311b4a97f399244d4d284a36dbe1abf2d
xilder <olabowaleayobami@gmail.com>
2023-11-14 10:20:16 -0800

    adds 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+ * Represents a Square
+ */
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+
+module.exports = Square;
