15d0d0ab314a4200fc920890725ff6b3930d7273
xilder <olabowaleayobami@gmail.com>
2023-11-14 08:32:43 -0800

    adds 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,6 +20,17 @@ class Rectangle {
       console.log(line);
     }
   }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
 }
 
 module.exports = Rectangle;
