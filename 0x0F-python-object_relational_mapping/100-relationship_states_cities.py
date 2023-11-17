2e55bed5b7ecd7a23315a7caef8d2e059dd0efd1
xilder <olabowaleayobami@gmail.com>
2023-11-17 14:16:25 -0800

    updates 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,6 +11,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3])
