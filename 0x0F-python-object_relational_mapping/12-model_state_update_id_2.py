72ac20b6d603a6bedd395dfb056814e4458cfa3b
xilder <olabowaleayobami@gmail.com>
2023-11-17 10:32:21 -0800

    adds  12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -7,6 +9,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
 
+    """
+    changes the name of a State object from the database hbtn_0e_6_usa
+    """
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3])
     engine = create_engine(db_url)
