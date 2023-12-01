fd0b58d670e1e4cffe170f65622ae0fd8e925221
xilder <olabowaleayobami@gmail.com>
2023-12-01 12:11:30 -0800

    adds 1-hbtn_header.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == "__main__":
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
-        body  = response.read()
+        body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
         print(f"\t- content: {body}")
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id variable found in the header of the response.
+"""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
