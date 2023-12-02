376b7cfeea7aae5b4ebc62a2bc0a3bdf30aba725
xilder <olabowaleayobami@gmail.com>
2023-12-02 08:32:55 -0800

    updates ./7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     response = requests.get(argv[1])
-    if response.status_code >= 400: 
+    if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
     else:
         print(response.text)
