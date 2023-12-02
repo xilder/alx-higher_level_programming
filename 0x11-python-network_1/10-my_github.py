a22fab615c780a18b3aed9c26ba87ec2cefdb0e0
xilder <olabowaleayobami@gmail.com>
2023-12-02 04:10:39 -0800

    adds 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(argv[1], argv[2])).json()
+    print(response.get("id"))
